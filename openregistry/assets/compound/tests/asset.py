c793d6a37c2d0f887f4d24ea1ac2a05e579f9c35
Scandie <grizly.vl@gmail.com>
2017-09-11 14:03:45 +0300

    Move simple_add_test to assets.core package
    - Remove assets_blanks module

--- openregistry/assets/compound/tests/asset.py
+++ openregistry/assets/compound/tests/asset.py
@@ -1,36 +1,25 @@
 # -*- coding: utf-8 -*-
-import os
 import unittest
 
-from openregistry.api.tests.base import BaseWebTest, snitch
 from openregistry.api.tests.blanks.mixins import ResourceTestMixin
 
 from openregistry.assets.core.tests.blanks.mixins import AssetResourceTestMixin
 
+from openregistry.assets.compound.models import Asset as AssetCompound
 from openregistry.assets.compound.tests.base import (
     test_asset_compound_data, BaseAssetWebTest
 )
-from openregistry.assets.compound.tests.asset_blanks import (
-    # AssetTest
-    simple_add_asset
-)
-
-
-class AssetCompoundTest(BaseWebTest):
-    initial_data = test_asset_compound_data
-    relative_to = os.path.dirname(__file__)
-
-    test_simple_add_asset = snitch(simple_add_asset)
 
 
 class AssetCompoundResourceTest(BaseAssetWebTest, ResourceTestMixin, AssetResourceTestMixin):
+    asset_model = AssetCompound
+    initial_data = test_asset_compound_data
     initial_status = 'pending'
 
 
 def suite():
     tests = unittest.TestSuite()
     tests.addTest(unittest.makeSuite(AssetCompoundResourceTest))
-    tests.addTest(unittest.makeSuite(AssetCompoundTest))
     return tests
 
 
