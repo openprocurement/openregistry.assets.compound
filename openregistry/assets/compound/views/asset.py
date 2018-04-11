897980f17d2c73e98d0ed1451004ba7c37329a49
Oleksiy Veretiuk <oleksiy.veretiuk@gmail.com>
2018-04-11 20:10:18 +0300

    Use managers except of logic in methods

--- openregistry/assets/compound/views/asset.py
+++ openregistry/assets/compound/views/asset.py
@@ -2,10 +2,43 @@
 from openregistry.assets.core.views.mixins import AssetResource
 from openregistry.assets.core.utils import opassetsresource
 
+# -*- coding: utf-8 -*-
+from openprocurement.api.validation import (
+    validate_change_status,
+)
+from openprocurement.api.utils import (
+    json_view
+)
+
+from openregistry.assets.core.validation import (
+    validate_patch_asset_data,
+    validate_data_by_model,
+    validate_asset_data
+)
+from openregistry.assets.compound.interfaces import IAssetManager
+
+
+patch_asset_validators = (
+    validate_patch_asset_data,
+    validate_change_status,
+    validate_data_by_model
+)
+
 
 @opassetsresource(name='compound:Asset',
                   path='/assets/{asset_id}',
                   assetType='compound',
                   description="Open Contracting compatible data exchange format.")
 class AssetCompoundResource(AssetResource):
-    pass
+
+    @json_view(content_type="application/json", permission='create_asset', validators=(validate_asset_data,))
+    def post(self):
+        """This API request is targeted to creating new Asset."""
+        return self.request.registry.getAdapter(self.context, IAssetManager).create_asset(self.request, self.LOGGER, self.db, self.server_id)
+
+
+    @json_view(content_type="application/json",
+               validators=patch_asset_validators,
+               permission='edit_asset')
+    def patch(self):
+        return self.request.registry.getAdapter(self.context, IAssetManager).change_asset(self.request, self.LOGGER)
--- openregistry/assets/compound/views/asset_document.py
+++ openregistry/assets/compound/views/asset_document.py
@@ -1,6 +1,21 @@
 # -*- coding: utf-8 -*-
 from openregistry.assets.core.views.mixins import AssetDocumentResource
 from openregistry.assets.core.utils import opassetsresource
+from openprocurement.api.utils import (
+    json_view,
+)
+from openprocurement.api.validation import (
+    validate_file_upload,
+    validate_document_data,
+    validate_patch_document_data,
+)
+from openregistry.assets.core.validation import (
+    validate_document_operation_in_not_allowed_asset_status,
+    validate_asset_document_update_not_by_author_or_asset_owner,
+)
+from openregistry.assets.compound.interfaces import (
+    IAssetDocumentManager
+)
 
 
 @opassetsresource(name='compound:Asset Documents',
@@ -9,4 +24,37 @@ from openregistry.assets.core.utils import opassetsresource
                   assetType='compound',
                   description="Asset related binary files (PDFs, etc.)")
 class AssetCompoundDocumentResource(AssetDocumentResource):
-    pass
+    @json_view(permission='view_asset')
+    def collection_get(self):
+        """Asset Documents List"""
+        return self.request.registry.getAdapter(self.context, IAssetDocumentManager).get_asset_documents(self.request)
+
+    @json_view(content_type="application/json", permission='upload_asset_documents',
+               validators=(
+                    validate_file_upload,
+                    validate_document_operation_in_not_allowed_asset_status))
+    def collection_post(self):
+        """Asset Document Upload"""
+        return self.request.registry.getAdapter(self.context, IAssetDocumentManager).add_asset_document(self.request, self.LOGGER)
+
+    @json_view(permission='view_asset')
+    def get(self):
+        """Asset Document Read"""
+        return self.request.registry.getAdapter(self.request.validated['asset'], IAssetDocumentManager).get_asset_document(self.request)
+
+    @json_view(content_type="application/json", permission='upload_asset_documents',
+               validators=(
+                    validate_document_data,
+                    validate_document_operation_in_not_allowed_asset_status,
+                    validate_asset_document_update_not_by_author_or_asset_owner))
+    def put(self):
+        """Asset Document Update"""
+        return self.request.registry.getAdapter(self.request.validated['asset'], IAssetDocumentManager).put_asset_document(self.request, self.LOGGER)
+
+    @json_view(content_type="application/json", permission='upload_asset_documents',
+               validators=(
+                    validate_patch_document_data,
+                    validate_document_operation_in_not_allowed_asset_status, validate_asset_document_update_not_by_author_or_asset_owner))
+    def patch(self):
+        """Asset Document Update"""
+        return self.request.registry.getAdapter(self.request.validated['asset'], IAssetDocumentManager).patch_asset_document(self.request, self.LOGGER)
