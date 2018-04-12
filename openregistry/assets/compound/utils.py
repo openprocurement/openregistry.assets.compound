1560d64cc15a7eaf7bd5700c6115c3de5cb4bf0a
Oleksiy Veretiuk <oleksiy.veretiuk@gmail.com>
2018-04-12 11:47:55 +0300

    Move saving object to view

--- openregistry/assets/compound/utils.py
+++ openregistry/assets/compound/utils.py
@@ -7,34 +7,23 @@ from openregistry.assets.core.events import AssetInitializeEvent
 from openprocurement.api.utils import (
     get_now,
     generate_id,
-    json_view,
     set_ownership,
-    context_unpack,
-    get_file,
-    update_file_content_type
 )
 
 from openregistry.assets.core.utils import (
-    save_asset,
-    asset_serialize,
     generate_asset_id
 )
 
 
-def change_asset(request, context, logger):
+def change_asset(request, context):
     asset = context
     if asset.status == 'active' and request.validated['data'].get('status') == 'pending':
         request.validated['data']['relatedLot'] = None
         request.validated['asset'].relatedLot = None
-    apply_patch(request, src=request.validated['asset_src'])
-    logger.info(
-        'Updated asset {}'.format(asset.id),
-        extra=context_unpack(request, {'MESSAGE_ID': 'asset_patch'})
-    )
-    return {'data': asset.serialize(asset.status)}
+    return asset
 
 
-def create_asset(request, context, logger, db, server_id):
+def create_asset(request, context, db, server_id):
     asset_id = generate_id()
     context.id = asset_id
     if not context.get('assetID'):
@@ -45,32 +34,14 @@ def create_asset(request, context, logger, db, server_id):
     acc = set_ownership(context, request)
     request.validated['asset'] = context
     request.validated['asset_src'] = {}
-    if save_asset(request):
-        logger.info('Created asset {} ({})'.format(asset_id, context.assetID),
-                         extra=context_unpack(logger.request, {'MESSAGE_ID': 'asset_create'},
-                                              {'asset_id': asset_id, 'assetID': context.assetID}))
-        request.response.status = 201
-        request.response.headers[
-            'Location'] = request.route_url('{}:Asset'.format(context.assetType), asset_id=asset_id)
-        return {
-            'data': context.serialize(context.status),
-            'access': acc
-        }
+    return context, acc
 
 
-def add_document(request, context, logger):
+def add_document(request, context):
     document = request.validated['document']
     document.author = request.authenticated_role
     context.documents.append(document)
-    if save_asset(request):
-        logger.info('Created asset document {}'.format(document.id),
-                         extra=context_unpack(request, {'MESSAGE_ID': 'asset_document_create'},
-                                              {'document_id': document.id}))
-        request.response.status = 201
-        document_route = request.matched_route.name.replace("collection_", "")
-        request.response.headers['Location'] = request.current_route_url(_route_name=document_route,
-                                                                                   document_id=document.id, _query={})
-        return {'data': document.serialize("view")}
+    return document
 
 
 def get_all_documents(request, context):
@@ -81,12 +52,10 @@ def get_all_documents(request, context):
             (i.id, i.serialize("view"))
             for i in context.documents
         ]).values(), key=lambda i: i['dateModified'])
-    return {'data': collection_data}
+    return collection_data
 
 
 def get_document(request):
-    if request.params.get('download'):
-        return get_file(request)
     document = request.validated['document']
     document_data = document.serialize("view")
     document_data['previousVersions'] = [
@@ -94,22 +63,13 @@ def get_document(request):
         for i in request.validated['documents']
         if i.url != document.url
     ]
-    return {'data': document_data}
+    return document_data
 
 
-def patch_document(request, logger):
-    if apply_patch(request, src=request.context.serialize()):
-        update_file_content_type(request)
-        logger.info('Updated asset document {}'.format(request.context.id),
-                         extra=context_unpack(request, {'MESSAGE_ID': 'asset_document_patch'}))
-        return {'data': request.context.serialize("view")}
+def patch_document(request):
+    pass
 
-
-def put_document(request, logger):
+def put_document(request):
     document = request.validated['document']
     request.validated['asset'].documents.append(document)
-    if save_asset(request):
-        logger.info('Updated asset document {}'.format(request.context.id),
-                         extra=context_unpack(request, {'MESSAGE_ID': 'asset_document_put'}))
-        return {'data': document.serialize("view")}
-
+    return document
--- openregistry/assets/compound/views/asset_document.py
+++ openregistry/assets/compound/views/asset_document.py
@@ -3,18 +3,23 @@ from openregistry.assets.core.views.mixins import AssetDocumentResource
 from openregistry.assets.core.utils import opassetsresource
 from openprocurement.api.utils import (
     json_view,
+    get_now,
+    generate_id,
+    json_view,
+    set_ownership,
+    context_unpack,
+    get_file,
+    update_file_content_type
 )
-from openprocurement.api.validation import (
-    validate_file_upload,
-    validate_document_data,
-    validate_patch_document_data,
-)
-from openregistry.assets.core.validation import (
-    validate_document_operation_in_not_allowed_asset_status,
-    validate_asset_document_update_not_by_author_or_asset_owner,
+from openregistry.assets.core.utils import (
+    save_asset,
+    asset_serialize,
+    generate_asset_id,
+    apply_patch,
+
 )
 from openregistry.assets.compound.interfaces import (
-    IAssetDocumentManager
+    IAssetManager
 )
 
 
@@ -27,34 +32,62 @@ class AssetCompoundDocumentResource(AssetDocumentResource):
     @json_view(permission='view_asset')
     def collection_get(self):
         """Asset Documents List"""
-        return self.request.registry.getAdapter(self.context, IAssetDocumentManager).get_asset_documents(self.request)
+        data = self.request.registry.getAdapter(
+            self.context,
+            IAssetManager
+        ).get_asset_documents(self.request)
+        return {'data': data}
 
-    @json_view(content_type="application/json", permission='upload_asset_documents',
-               validators=(
-                    validate_file_upload,
-                    validate_document_operation_in_not_allowed_asset_status))
+    @json_view(content_type="application/json", permission='upload_asset_documents')
     def collection_post(self):
         """Asset Document Upload"""
-        return self.request.registry.getAdapter(self.context, IAssetDocumentManager).add_asset_document(self.request, self.LOGGER)
+        document = self.request.registry.getAdapter(
+            self.context,
+            IAssetManager
+        ).add_asset_document(self.request)
+        if save_asset(self.request):
+            self.LOGGER.info('Created asset document {}'.format(document.id),
+                             extra=context_unpack(self.request, {'MESSAGE_ID': 'asset_document_create'},
+                                                  {'document_id': document.id}))
+            self.request.response.status = 201
+            document_route = self.request.matched_route.name.replace("collection_", "")
+            self.request.response.headers['Location'] = self.request.current_route_url(_route_name=document_route,
+                                                                                       document_id=document.id, _query={})
+            return {'data': document.serialize("view")}
+
 
     @json_view(permission='view_asset')
     def get(self):
         """Asset Document Read"""
-        return self.request.registry.getAdapter(self.request.validated['asset'], IAssetDocumentManager).get_asset_document(self.request)
+        if self.request.params.get('download'):
+            return get_file(self.request)
+        data = self.request.registry.getAdapter(
+            self.request.validated['asset'],
+            IAssetManager
+        ).get_asset_document(self.request)
+        return {'data': data}
 
-    @json_view(content_type="application/json", permission='upload_asset_documents',
-               validators=(
-                    validate_document_data,
-                    validate_document_operation_in_not_allowed_asset_status,
-                    validate_asset_document_update_not_by_author_or_asset_owner))
+    @json_view(content_type="application/json", permission='upload_asset_documents')
     def put(self):
         """Asset Document Update"""
-        return self.request.registry.getAdapter(self.request.validated['asset'], IAssetDocumentManager).put_asset_document(self.request, self.LOGGER)
+        document = self.request.registry.getAdapter(
+            self.request.validated['asset'],
+            IAssetManager
+        ).put_asset_document(self.request)
+        if save_asset(self.request):
+            self.LOGGER.info('Updated asset document {}'.format(self.request.context.id),
+                        extra=context_unpack(self.request, {'MESSAGE_ID': 'asset_document_put'}))
+            return {'data': document.serialize("view")}
 
-    @json_view(content_type="application/json", permission='upload_asset_documents',
-               validators=(
-                    validate_patch_document_data,
-                    validate_document_operation_in_not_allowed_asset_status, validate_asset_document_update_not_by_author_or_asset_owner))
+    @json_view(content_type="application/json", permission='upload_asset_documents')
     def patch(self):
         """Asset Document Update"""
-        return self.request.registry.getAdapter(self.request.validated['asset'], IAssetDocumentManager).patch_asset_document(self.request, self.LOGGER)
+        self.request.registry.getAdapter(
+            self.request.validated['asset'],
+            IAssetManager
+        ).patch_asset_document(self.request)
+
+        if apply_patch(self.request, src=self.request.context.serialize()):
+            self.LOGGER.info('Updated asset document {}'.format(self.request.context.id),
+                        extra=context_unpack(self.request, {'MESSAGE_ID': 'asset_document_patch'}))
+            return {'data': self.request.context.serialize("view")}
