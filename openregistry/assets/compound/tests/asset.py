3f88a52968422dd99793734705869a08757f01c6
Oleksandr Vinnichuk <oleksandr.v@quintagroup.org>
2018-04-16 19:17:33 +0300

    Add tests for 341 shema

--- openregistry/assets/compound/tests/asset.py
+++ openregistry/assets/compound/tests/asset.py
@@ -11,7 +11,7 @@ from openregistry.assets.core.tests.blanks.mixins import (
 
 from openregistry.assets.compound.models import Asset as AssetCompound
 from openregistry.assets.compound.tests.base import (
-    test_asset_compound_data, BaseAssetWebTest
+    test_asset_compound_data, test_asset_compound_data_341, BaseAssetWebTest
 )
 
 
@@ -37,8 +37,38 @@ class AssetCompoundResourceTest(BaseAssetWebTest, ResourceTestMixin, AssetResour
         self.assertEqual(response['items'][0]['unit'], test_asset_compound_data['items'][0]['unit'])
         self.assertEqual(response['items'][0]['quantity'], test_asset_compound_data['items'][0]['quantity'])
 
+    def test_create_compount_with_item_schemas_341(self):
+        response = self.app.post_json('/', {'data': test_asset_compound_data_341})
+        self.assertEqual(response.status, '201 Created')
+        self.assertEqual(response.content_type, 'application/json')
+        response = response.json['data']
+        self.assertEqual(response['items'][0]['schema_properties']['properties'], test_asset_compound_data_341['items'][0]['schema_properties']['properties'])
+        self.assertEqual(response['items'][0]['schema_properties']['code'][0:2], test_asset_compound_data_341['items'][0]['schema_properties']['code'][:2])
+        self.assertEqual(response['items'][0]['description'], test_asset_compound_data_341['items'][0]['description'])
+        self.assertEqual(response['items'][0]['classification'], test_asset_compound_data_341['items'][0]['classification'])
+        self.assertEqual(response['items'][0]['additionalClassifications'], test_asset_compound_data_341['items'][0]['additionalClassifications'])
+        self.assertEqual(response['items'][0]['address'], test_asset_compound_data_341['items'][0]['address'])
+        self.assertEqual(response['items'][0]['id'], test_asset_compound_data_341['items'][0]['id'])
+        self.assertEqual(response['items'][0]['unit'], test_asset_compound_data_341['items'][0]['unit'])
+        self.assertEqual(response['items'][0]['quantity'], test_asset_compound_data_341['items'][0]['quantity'])
+
     def test_bad_item_schemas_code(self):
         bad_initial_data = deepcopy(test_asset_compound_data)
+        bad_initial_data['items'][0]['classification']['id'] = "42124210-6"
+        response = self.app.post_json('/', {'data': bad_initial_data},status=422)
+        self.assertEqual(response.status, '422 Unprocessable Entity')
+        self.assertEqual(response.content_type, 'application/json')
+        self.assertEqual(response.json['errors'],
+                         [{
+                             "location": "body",
+                             "name": "items",
+                             "description": [
+                                 {u"schema_properties": [u"classification id mismatch with schema_properties code"]},
+                                 {u"schema_properties": [u"classification id mismatch with schema_properties code"]},
+                             ]
+                         }])
+    def test_bad_item_schemas_code_341(self):
+        bad_initial_data = deepcopy(test_asset_compound_data_341)
         bad_initial_data['items'][0]['classification']['id'] = "42124210-6"
         response = self.app.post_json('/', {'data': bad_initial_data},status=422)
         self.assertEqual(response.status, '422 Unprocessable Entity')
@@ -63,6 +93,20 @@ class AssetCompoundResourceTest(BaseAssetWebTest, ResourceTestMixin, AssetResour
         status = resource['status']
         self.set_status(self.initial_status)
 
+        response = self.app.patch_json('/{}?access_token={}'.format(
+                                self.resource_id, self.resource_token),
+                                headers=self.access_header,
+                                params={'data': {"items": [{'schema_properties': None}, {'schema_properties': None}]}})
+
+    def test_delete_item_schema_341(self):
+        response = self.app.post_json('/', {'data': test_asset_compound_data_341})
+        self.assertEqual(response.status, '201 Created')
+        resource = response.json['data']
+        self.resource_token = response.json['access']['token']
+        self.access_header = {'X-Access-Token': str(response.json['access']['token'])}
+        self.resource_id = resource['id']
+        status = resource['status']
+        self.set_status(self.initial_status)
         response = self.app.patch_json('/{}?access_token={}'.format(
                                 self.resource_id, self.resource_token),
                                 headers=self.access_header,
