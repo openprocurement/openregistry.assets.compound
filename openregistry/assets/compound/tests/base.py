a86e41fc6fd2ab206123605ef998d91630b0aaf9
vitalii-motso <vitalii.motso@gmail.com>
2018-06-19 09:27:14 +0300

    add mock config for testing

--- openregistry/assets/compound/tests/base.py
+++ openregistry/assets/compound/tests/base.py
@@ -4,15 +4,23 @@ from copy import deepcopy
 
 from openregistry.assets.core.tests.base import (
     BaseAssetWebTest as BaseAWT,
+    connection_mock_config,
+    MOCK_CONFIG as BASE_MOCK_CONFIG,
     test_asset_compound_data,
     test_asset_compound_data_341,
     test_asset_compound_data_060
 )
 
+from openregistry.assets.compound.tests.fixtures import PARTIAL_MOCK_CONFIG
+MOCK_CONFIG = connection_mock_config(PARTIAL_MOCK_CONFIG,
+                                     base=BASE_MOCK_CONFIG,
+                                     connector=('plugins', 'api', 'plugins',
+                                                'assets.core', 'plugins'))
 
 class BaseAssetWebTest(BaseAWT):
     initial_auth = ('Basic', ('broker', ''))
     relative_to = os.path.dirname(__file__)
+    mock_config = MOCK_CONFIG
 
     def setUp(self):
         self.initial_data = deepcopy(test_asset_compound_data)
@@ -22,3 +30,4 @@ class BaseAssetWebTest(BaseAWT):
 class AssetContentWebTest(BaseAssetWebTest):
     init = True
     initial_status = 'pending'
+    mock_config = MOCK_CONFIG
---
+++ openregistry/assets/compound/tests/fixtures.py
@@ -0,0 +1,6 @@
+PARTIAL_MOCK_CONFIG = {
+    "assets.compound":{
+        "use_default":True,
+        "aliases":["compound1"]
+    }
+}
