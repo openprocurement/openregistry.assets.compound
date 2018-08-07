0c51c74940b1c8f324b1db8dc33194a12bdb12e4
vitalii-motso <vitalii.motso@gmail.com>
2018-08-07 15:45:13 +0300

    rm level of accreditation from models

--- openregistry/assets/compound/tests/fixtures.py
+++ openregistry/assets/compound/tests/fixtures.py
@@ -1,6 +1,10 @@
 PARTIAL_MOCK_CONFIG = {
-    "assets.compound":{
-        "use_default":True,
-        "aliases":["compound1"]
+    "assets.compound": {
+        "use_default": True,
+        "aliases": ["compound1"],
+        "accreditation": {
+            "create": [1],
+            "edit": [2]
+        }
     }
 }
